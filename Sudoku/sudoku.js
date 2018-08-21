3f317d51754ec4bd23ee67ca2acdd6db61d49665
Romain <romain-legoff@hotmail.fr>
2018-08-21 21:59:22 +0200

    SUDOKU|Add function to shuffle the sudoku by difficulty

--- Sudoku/sudoku.js
+++ Sudoku/sudoku.js
@@ -5,15 +5,35 @@ for (var i = 0; i < sudoku.length; i++) {
     sudoku[i] = new Array(9);
 }
 
-// Sample array initialization
-var sample = new Array(9);
+// Sudoku shuffled initialization
+var sudokuShuffled = new Array(9);
+
+for (var i = 0; i < sudokuShuffled.length; i++) {
+    sudokuShuffled[i] = new Array(9);
+}
+
+// Samples array initialization
+var sampleNumbers = new Array(9);
+var sampleGridSize = new Array(9);
+
+for (var i = 0; i < sampleGridSize.length; i++) {
+    sampleGridSize[i] = new Array(9);
+}
 
 /**
- * Fills the sample
+ * Fills the samples
  */
-function fillSample() {
+function fillSampleNumbers() {
     for (var i = 0; i < 9; i++) {
-        sample[i] = i + 1;
+        sampleNumbers[i] = i + 1;
+    }
+}
+
+function fillSampleGridSize() {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; i < 9; i++) {
+            sampleGridSize[i][j] = 1;
+        }
     }
 }
 
@@ -23,16 +43,16 @@ function fillSample() {
 function fillSudoku() {
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 9; j++) {
-            fillSample();
+            fillSampleNumbers();
             var caseFilled = false;
-            while (sample.length != 0 && !caseFilled) {
-                var random = Math.floor((Math.random() * sample.length));
-                var number = sample[random];
+            while (sampleNumbers.length != 0 && !caseFilled) {
+                var random = Math.floor((Math.random() * sampleNumbers.length));
+                var number = sampleNumbers[random];
                 if (checkRow(j, i, number) && checkCol(j, i, number) && checkBox(j, i, number)) {
                     sudoku[i][j] = number;
                     caseFilled = true;
                 }
-                sample.splice(random, 1);
+                sampleNumbers.splice(random, 1);
             }
             if (sudoku[i][j] == null) {
                 return false;
@@ -48,7 +68,7 @@ var iteration = 0;
 /**
  * Fills the sudoku, and do it again until it's not completely full
  */
-while (!completelyFilled) {
+while (!completelyFilled && iteration < 1000) {
     completelyFilled = fillSudoku();
     iteration++;
 }
@@ -108,6 +128,25 @@ function checkBox(x, y, number) {
     return true;
 }
 
+function shuffleSudoku(difficulty, sudoku) {
+    this.sudokuShuffled = sudoku;
+    var randomFound = false;
+    for (var h = 0; h < difficulty; h++) {
+        var randomFound = false;
+        while (!randomFound) {
+            var i = Math.floor((Math.random() * sampleGridSize.length));
+            var j = Math.floor((Math.random() * sampleGridSize[i].length));
+            if (sampleGridSize[i][j] != 0) {
+                sudokuShuffled[i][j] = "";
+                sampleGridSize[i][j] = 0;
+                randomFound = true;
+            }
+        }
+    }
+}
+
+fillSampleGridSize();
+shuffleSudoku(65, sudoku);
 
 // Modify the DOM to display the sudoku
 for (var i = 0; i < 9; i++) {
@@ -115,8 +154,8 @@ for (var i = 0; i < 9; i++) {
         var col = j + 1;
         var row = i + 1;
         var id = "col" + col + "_" + "row" + row;
-        document.getElementById(id).setAttribute("value", sudoku[i][j]);
-        if (sudoku[i][j] != null) {
+        document.getElementById(id).setAttribute("value", sudokuShuffled[i][j]);
+        if (sudokuShuffled[i][j] != "") {
             document.getElementById(id).setAttribute("disabled", true);
         }
     }
